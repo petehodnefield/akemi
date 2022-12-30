21c7665d0ce7a0c8f8361c0b275f6e43e718df4d
petehodnefield <98546095+petehodnefield@users.noreply.github.com>
2022-12-30 15:46:43 -0600

    added form confirmation

--- src/pages/Home/components/Form.js
+++ src/pages/Home/components/Form.js
@@ -1,17 +1,35 @@
+import { useState, useEffect, useRef } from "react";
 import React from "react";
 
 const Form = () => {
+  const [formSubmit, setFormSubmit] = useState(false);
+  const [message, setMessage] = useState(
+    "thank you for joining my newsletter ❤️  "
+  );
+  const formRef = useRef();
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    formRef.current?.reset();
+    setFormSubmit(true);
+    // reset message
+    setTimeout(() => {
+      setMessage("");
+    }, 6000);
+  };
+
   return (
     <section className="form-section">
       <div className="margin form">
         <h3 className="m-1">Subscribe</h3>
-        <form>
+        <form ref={formRef} id="form" onSubmit={(e) => handleFormSubmit(e)}>
           <div className="form-input-wrapper m-p5">
             <label className="bold m-p25">Email address</label>
             <input
               className="bold form-input"
               required
               maxLength="30"
+              minLength="6"
               type="email"
               name="email"
             ></input>
@@ -20,6 +38,11 @@ const Form = () => {
             Submit
           </button>
         </form>
+        {formSubmit && message ? (
+          <p className="p2 form-message">{message}</p>
+        ) : (
+          ""
+        )}
       </div>
     </section>
   );
